e46eac8eb8823e1a466368d8e3f25f72ef69979d
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-07-05 14:41:51 +0000

    Add ternary operators to setState

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -5,7 +5,7 @@ class StatefulGreeting extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            introduction: "Hello",
+            introduction: "Hello!",
             buttonText: "Exit",
         };
     }
@@ -13,7 +13,14 @@ class StatefulGreeting extends React.Component {
     handleClick() {
         this.setState(
             {
-                introduction: "Goodbye!",
+                // use ternary operator to change the state based on the current state
+                // If state of introduction is "Hello!" then set to "Goodbye!" else set to "Hello!"
+                introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                // If state of buttonText is "Exit" then set to "Enter" else set to "Exit"
+                buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+            }, () => { // callback function passed as second arguement to setState, that will run after state update is complete
+                console.log('New state: ', this.state.introduction);
+                console.log('New state: ', this.state.buttonText);
             }
         );
     }
@@ -23,9 +30,9 @@ class StatefulGreeting extends React.Component {
         return (
             <div>
                 <h1>{this.state.introduction}, {this.props.greeting}</h1>
-                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>  
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
-            
+
         )
     }
 }
