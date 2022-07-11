7d57c8a7f892b89e2cafc8191f8acbc965426e1c
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-07-11 14:35:21 +0000

    Add MethodsAsProps files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,8 @@ import EventsFunctional from './components/EventsFunctional';
 import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
@@ -17,8 +19,10 @@ function App() {
       {/* <EventsFunctional /> */}
       {/* <EventsClass /> */}
       {/* <EventBinding /> */}
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true} />
+      {/* <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={true} /> */}
+      {/* <NestingComponents /> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
